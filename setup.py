c9989934dfde5132c9d34dfdfbc7886d91d2c3e6
yasufumi <yasufumi.taniguchi@gmail.com>
2019-03-15 10:51:34 +0900

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='lineflow',
-    version='0.2.1',
+    version='0.2.2',
     description='Framework-Agnostic NLP Data Loader in Python',
     long_description=open('./README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
