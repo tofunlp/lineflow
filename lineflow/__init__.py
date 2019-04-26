79e3c217286132df9be2f800a98af1fcc78c4c93
yasufumi <yasufumi.taniguchi@gmail.com>
2019-04-26 15:10:25 +0900

    Move Dataset.load to lineflow.load

--- lineflow/__init__.py
+++ lineflow/__init__.py
@@ -5,3 +5,4 @@ from lineflow.core import lineflow_concat as concat  # NOQA
 from lineflow.core import lineflow_zip as zip  # NOQA
 from lineflow.core import lineflow_filter as filter  # NOQA
 from lineflow.core import lineflow_flat_map as flat_map  # NOQA
+from lineflow.core import lineflow_load as load  # NOQA
