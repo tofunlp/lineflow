6819d18d9f1851833dacb81b0bdb82c7e58cc07c
yasufumi <yasufumi.taniguchi@gmail.com>
2019-05-09 13:11:32 +0900

    Update test cases for lineflow.TextDataset

--- tests/test_core.py
+++ tests/test_core.py
@@ -199,16 +199,28 @@ class DatasetTestCase(TestCase):
 
 class TextDatasetTestCase(TestCase):
 
-    @patch('lineflow.core.linecache.getline')
-    def test_text(self, linecache_getline_mock):
+    def setUp(self):
+        self.linecache_getline_patcher = patch('lineflow.core.linecache.getline')
+        self.linecache_getline_mock = self.linecache_getline_patcher.start()
         lines = ['This is a test .', 'That is also a test .']
-        linecache_getline_mock.side_effect = lambda filename, i: lines[i - 1]
+        self.linecache_getline_mock.side_effect = \
+            lambda filename, i: lines[-1] if i - 1 > len(lines) else lines[i - 1]
         fp = tempfile.NamedTemporaryFile()
         for x in lines:
             fp.write(f'{x}\n'.encode('utf-8'))
         fp.seek(0)
+        self.lines = lines
+        self.fp = fp
 
-        # single file
+    def tearDown(self):
+        self.linecache_getline_patcher.stop()
+        self.fp.close()
+
+    def test_text(self):
+        fp = self.fp
+        lines = self.lines
+        linecache_getline_mock = self.linecache_getline_mock
+
         data = TextDataset(fp.name)
         for x, y in zip(data, lines):
             self.assertEqual(x, y)
@@ -233,22 +245,46 @@ class TextDatasetTestCase(TestCase):
 
         self.assertIsInstance(data, lineflow.core.MapDataset)
 
-        # multiple file
-        data = TextDataset([fp.name, fp.name])
+    def test_zips_multiple_files(self):
+        fp = self.fp
+        lines = self.lines
+        linecache_getline_mock = self.linecache_getline_mock
+
+        data = TextDataset([fp.name, fp.name], mode='zip')
         for x, y in zip(data, lines):
             self.assertTupleEqual(x, (y, y))
         for j, y in enumerate(lines):
             self.assertTupleEqual(data[j], (y, y))
             linecache_getline_mock.called_once_with(fp.name, j + 1)
         self.assertEqual(linecache_getline_mock.call_count,
-                         i + 1 + (j + 1) * len(lines))
+                         (j + 1) * len(lines))
         self.assertEqual(data._length, None)
         self.assertEqual(len(data), len(lines))
         self.assertEqual(data._length, len(lines))
         # check if length is cached
         self.assertEqual(len(data), len(lines))
 
-        fp.close()
+    def test_concats_multiple_files(self):
+        fp = self.fp
+        lines = self.lines
+        linecache_getline_mock = self.linecache_getline_mock
+
+        data = TextDataset([fp.name, fp.name], mode='concat')
+        for x, y in zip(data, lines + lines):
+            self.assertEqual(x, y)
+        for j, y in enumerate(lines + lines):
+            self.assertEqual(data[j], y)
+            linecache_getline_mock.called_once_with(fp.name, j + 1)
+        self.assertEqual(linecache_getline_mock.call_count,
+                         len(lines) * 2)
+        self.assertEqual(len(data), len(lines) * 2)
+        self.assertEqual(data._length, len(lines) * 2)
+
+        self.assertEqual(data[len(data)], lines[-1])
+
+    def test_raises_value_error_with_invalid_mode(self):
+        with self.assertRaises(ValueError):
+            TextDataset([self.fp.name, self.fp.name], mode='invalid_mode')
 
 
 class CsvDatasetTestCase(TestCase):
