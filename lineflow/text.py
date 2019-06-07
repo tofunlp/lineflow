65f403fad9694ec97aa76a1083ceaded125866b0
yasufumi <yasufumi.taniguchi@gmail.com>
2019-06-07 16:20:46 +0900

    Add lazier evaluation support for lineflow.text

--- lineflow/text.py
+++ lineflow/text.py
@@ -1,7 +1,6 @@
-from typing import Union, List, Tuple, Dict, Iterator
+from typing import Union, List, Dict, Iterator
 import os
 import os.path as osp
-import linecache
 import csv
 import mmap
 
@@ -9,40 +8,51 @@ from lineflow import Dataset
 from lineflow.core import ConcatDataset, ZipDataset
 
 
-def count_lines(filepath: str, encoding: str = 'utf-8') -> int:
-    count = 0
-    with open(filepath, mode='r+', encoding=encoding) as f:
-        mm = mmap.mmap(f.fileno(), 0)
-        while mm.readline():
-            count += 1
-    return count
+class RandomAccessFile:
+    def __init__(self, path: str, encoding: str = 'utf-8') -> None:
+        assert osp.exists(path)
+
+        self._path = path
+        self._encoding = encoding
+        self._offsets = None
+        self._length = None
+
+    def _initialize_offsets(self) -> None:
+        with open(self._path, 'r+', encoding=self._encoding) as f:
+            mm = mmap.mmap(f.fileno(), 0)
+            offsets = [0] + [mm.tell() for _ in iter(mm.readline, b'')]
+        self._offsets = offsets
+
+    def __iter__(self) -> Iterator[str]:
+        with open(self._path, encoding=self._encoding) as f:
+            for line in f:
+                yield line.rstrip(os.linesep)
+
+    def __getitem__(self, index: int) -> str:
+        if self._offsets is None:
+            self._initialize_offsets()
+        if index < 0 or len(self) <= index:
+            raise IndexError('RandomAccessTextFile object index out of range')
+        with open(self._path, encoding=self._encoding) as f:
+            f.seek(self._offsets[index])
+            return f.readline().rstrip(os.linesep)
+
+    def __len__(self) -> int:
+        if self._offsets is None:
+            self._initialize_offsets()
+        if self._length is None:
+            self._length = len(self._offsets) - 1
+        return self._length
 
 
 class SingleTextDataset(Dataset):
     def __init__(self,
-                 filepath: str,
+                 path: str,
                  encoding: str = 'utf-8') -> None:
-        self._filepath = filepath
+        super().__init__(RandomAccessFile(path, encoding))
 
-        assert osp.exists(filepath)
-
+        self._path = path
         self._encoding = encoding
-        self._length = None
-
-    def __iter__(self) -> Iterator[str]:
-        with open(self._filepath, encoding=self._encoding) as f:
-            for line in f:
-                yield line.rstrip(os.linesep)
-
-    def get_example(self, i: int) -> str:
-        return linecache.getline(self._filepath, i + 1).rstrip(os.linesep)
-
-    def get_length(self) -> int:
-        return count_lines(self._filepath, self._encoding)
-
-    @property
-    def _dataset(self) -> 'SingleTextDataset':
-        return self
 
 
 class ConcatTextDataset(ConcatDataset):
@@ -65,51 +75,41 @@ class ZipTextDataset(ZipDataset):
 
 class TextDataset(Dataset):
     def __init__(self,
-                 filepath: Union[str, List[str]],
+                 paths: Union[str, List[str]],
                  encoding: str = 'utf-8',
                  mode: str = 'zip') -> None:
-        if isinstance(filepath, str):
-            self._dataset = SingleTextDataset(filepath, encoding)
-        elif isinstance(filepath, list):
+        if isinstance(paths, str):
+            dataset = SingleTextDataset(paths, encoding)
+        elif isinstance(paths, list):
             if mode == 'zip':
-                self._dataset = ZipTextDataset(filepath, encoding)
+                dataset = ZipTextDataset(paths, encoding)
             elif mode == 'concat':
-                self._dataset = ConcatTextDataset(filepath, encoding)
+                dataset = ConcatTextDataset(paths, encoding)
             else:
                 raise ValueError(f"only 'zip' and 'concat' are valid for 'mode', but '{mode}' is given.")
 
-        self._filepath = filepath
-        self._length = self._dataset._length
-
-    def __iter__(self) -> Iterator[Union[str, Tuple[str]]]:
-        yield from self._dataset
-
-    def get_example(self, i: int) -> Union[str, Tuple[str]]:
-        return self._dataset[i]
-
-    def get_length(self) -> int:
-        return len(self._dataset)
+        super().__init__(dataset)
 
 
 class CsvDataset(SingleTextDataset):
     def __init__(self,
-                 filepath: str,
+                 path: str,
                  encoding: str = 'utf-8',
                  delimiter: str = ',',
                  header: bool = False) -> None:
 
-        super().__init__(filepath, encoding)
+        super().__init__(path, encoding)
 
         self._delimiter = delimiter
         self._reader = csv.DictReader if header else csv.reader
         if header:
-            with open(filepath, encoding=encoding) as f:
+            with open(path, encoding=encoding) as f:
                 self._header = next(csv.reader(f))
         else:
             self._header = None
 
     def __iter__(self) -> Iterator[Union[List[str], Dict[str, str]]]:
-        with open(self._filepath, encoding=self._encoding) as f:
+        with open(self._path, encoding=self._encoding) as f:
             if self._header is None:
                 yield from self._reader(f, delimiter=self._delimiter)
             else:
@@ -119,17 +119,17 @@ class CsvDataset(SingleTextDataset):
 
     def get_example(self, i: int) -> Union[List[str], Dict[str, str]]:
         if self._header is None:
-            row = self._reader([super().get_example(i)],
+            row = self._reader([self._dataset[i]],
                                delimiter=self._delimiter)
         else:
-            row = self._reader([super().get_example(i + 1)],
+            row = self._reader([self._dataset[i + 1]],
                                delimiter=self._delimiter,
                                fieldnames=self._header)
         return next(row)
 
     def get_length(self) -> int:
-        count = count_lines(self._filepath, self._encoding)
+        length = len(self._dataset)
         if self._header is None:
-            return count
+            return length
         else:
-            return count - 1
+            return length - 1
