a7dacfa8781773d244f1d9e1a9339e0e75b5aed8
yasufumi <yasufumi.taniguchi@gmail.com>
2019-06-08 15:46:57 +0900

    Refactor test cases for lineflow.core

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,7 +3,76 @@ from unittest.mock import patch, Mock
 
 import lineflow
 from lineflow import Dataset
+from lineflow.core import ConcatDataset, ZipDataset
 from lineflow.core import RandomAccessConcat, RandomAccessZip
+
+
+class RandomAccessConcatTestCase(TestCase):
+
+    def setUp(self):
+        self.n = 5
+        self.base = range(100)
+        self.data = RandomAccessConcat(*[self.base for _ in range(5)])
+
+    def test_init(self):
+        self.assertEqual(len(self.data._datasets), self.n)
+        self.assertIsNone(self.data._offsets)
+        self.assertIsNone(self.data._length)
+
+    def test_iter(self):
+        for x, y in zip(self.data, list(self.base) * self.n):
+            self.assertEqual(x, y)
+        self.assertIsNone(self.data._offsets)
+        self.assertIsNone(self.data._length)
+
+    def test_supports_random_access_lazily(self):
+        import itertools
+
+        self.assertIsNone(self.data._offsets)
+        self.assertSequenceEqual(self.data, list(self.base) * self.n)
+        expected_lengths = list(itertools.accumulate(len(self.base) for _ in range(self.n)))
+        self.assertListEqual(self.data._lengths, expected_lengths)
+        self.assertListEqual(self.data._offsets, [0] + expected_lengths[:-1])
+
+    def test_raises_index_error_with_invalid_index(self):
+        with self.assertRaises(IndexError):
+            self.data[len(self.data)]
+            self.data[-1]
+
+    def test_returns_length_lazily(self):
+        self.assertIsNone(self.data._length)
+        self.assertEqual(len(self.data), len(self.base) * self.n)
+        self.assertEqual(self.data._length, len(self.data))
+
+
+class RandomAccessZipTestCase(TestCase):
+
+    def setUp(self):
+        self.n = 5
+        self.base = range(100)
+        self.data = RandomAccessZip(*[self.base for _ in range(5)])
+
+    def test_init(self):
+        self.assertEqual(len(self.data._datasets), self.n)
+        self.assertIsNone(self.data._length)
+
+    def test_iter(self):
+        for x, y in zip(self.data, self.base):
+            self.assertEqual(x, tuple([y] * self.n))
+        self.assertIsNone(self.data._length)
+
+    def test_supports_random_access(self):
+        self.assertSequenceEqual(self.data, list(zip(*[self.base for _ in range(self.n)])))
+
+    def test_raises_index_error_with_invalid_index(self):
+        with self.assertRaises(IndexError):
+            self.data[len(self.data)]
+            self.data[-1]
+
+    def test_returns_lengths_lazily(self):
+        self.assertIsNone(self.data._length)
+        self.assertEqual(len(self.data), len(self.base))
+        self.assertEqual(self.data._length, len(self.data))
 
 
 class DatasetTestCase(TestCase):
@@ -13,9 +82,13 @@ class DatasetTestCase(TestCase):
         self.data = Dataset(self.base)
 
     def test_getitem(self):
-        for i, y in enumerate(self.base):
-            self.assertEqual(self.data[i], y)
-        self.assertEqual(self.data[10:20], list(self.base[10:20]))
+        self.assertSequenceEqual(self.data, self.base)
+
+    def test_supports_slicing(self):
+        slice1 = slice(10, 20)
+        slice2 = slice(0, 99)
+        self.assertListEqual(self.data[slice1], list(self.base[slice1]))
+        self.assertListEqual(self.data[slice2], list(self.base[slice2]))
 
     def test_len(self):
         self.assertEqual(len(self.data), len(self.base))
@@ -23,53 +96,38 @@ class DatasetTestCase(TestCase):
     def test_add(self):
         data = self.data + self.data + self.data
         expected = list(self.base) * 3
-        for i, (x, y) in enumerate(zip(data, expected)):
-            self.assertEqual(x, y)
-            self.assertEqual(data[i], y)
+        self.assertSequenceEqual(data, expected)
 
     def test_map(self):
-        def f(x):
-            return x ** 2
+        def f(x): return x ** 2
 
-        data = self.data.map(f)
+        self.assertSequenceEqual(
+            self.data.map(f),
+            list(map(f, self.base)))
 
-        for x, y in zip(data, self.base):
-            self.assertEqual(x, f(y))
+    def test_keeps_original_dataset_after_multiple_maps(self):
+        def f(x): return x
 
-        for i, y in enumerate(self.base):
-            self.assertEqual(data[i], f(y))
+        data = self.data
+        for i in range(100):
+            data = data.map(f)
+            self.assertEqual(data._dataset, self.base)
+            self.assertEqual(len(data._funcs), i + 1)
 
-        self.assertEqual(data._dataset, self.base)
-
-    def test_method_chain(self):
-        data = self.data.map(lambda x: x ** 2) \
-            .map(lambda x: x / 2)
-
-        expected = [x ** 2 / 2 for x in self.base]
-
-        for x, y in zip(data, expected):
-            self.assertEqual(x, y)
-
-        self.assertEqual(data._dataset, self.base)
+    def test_supports_method_chain(self):
+        data = self.data.map(lambda x: x ** 2).map(lambda x: x / 2)
+        self.assertSequenceEqual(
+            data, [x ** 2 / 2 for x in self.base])
 
     def test_all(self):
-        data = self.data
-        expected = list(self.base)
-
-        self.assertListEqual(data.all(), expected)
+        self.assertListEqual(self.data.all(), list(self.base))
 
     def test_first(self):
-        data = self.data.first()
-        expected = next(iter(self.base))
-
-        self.assertEqual(data, expected)
+        self.assertEqual(self.data.first(), self.base[0])
 
     def test_take(self):
         n = 50
-        data = self.data.take(n)
-        expected = list(self.base[:n])
-
-        self.assertListEqual(data, expected)
+        self.assertListEqual(self.data.take(n), list(self.base[:n]))
 
     @patch('lineflow.core.Path.open')
     @patch('lineflow.core.Path')
@@ -146,6 +204,7 @@ class DatasetTestCase(TestCase):
         path.open.assert_called_once_with('wb')
         pickle_dump_mock.assert_called_once_with(data.all(), fp)
         self.assertIsInstance(data, lineflow.core.CacheDataset)
+        self.assertListEqual(data._dataset, [x ** 2 for x in self.base])
 
         for i, x in enumerate(data):
             y = self.data[i] ** 2
@@ -197,60 +256,89 @@ class DatasetTestCase(TestCase):
             lineflow.Dataset.load(filepath)
 
 
-class MiscTestCase(TestCase):
+class LineflowConcatTestCase(TestCase):
 
     def setUp(self):
         self.base = range(100)
-        self.data = Dataset(self.base)
+        self.n = 5
+        self.data = lineflow.concat(*[Dataset(self.base)] * self.n)
 
-    def test_concat(self):
-        data = lineflow.concat(self.data, self.data)
-        expected = list(self.base) * 3
-        for i, (x, y) in enumerate(zip(data, expected)):
-            self.assertEqual(x, y)
-            self.assertEqual(data[i], y)
+    def test_returns_concat_dataset(self):
+        self.assertIsInstance(self.data, ConcatDataset)
+        self.assertIsInstance(self.data._dataset, RandomAccessConcat)
 
-        with self.assertRaises(IndexError):
-            data[len(data) * 3]
+    def test_keeps_original_dataset_after_multiple_maps(self):
+        def f(x): return x
 
-        result = data.map(lambda x: x ** 2).map(lambda x: x)
-        expected = [y ** 2 for y in expected]
-        for i, (x, y) in enumerate(zip(result, expected)):
-            self.assertEqual(x, y)
-            self.assertEqual(result[i], y)
-        self.assertIsInstance(result._dataset, RandomAccessConcat)
+        data = self.data
+        for i in range(100):
+            data = data.map(f)
+            self.assertIsInstance(data._dataset, RandomAccessConcat)
+            self.assertEqual(len(data._funcs), i + 1)
 
-    def test_zip(self):
-        data = lineflow.zip(self.data, self.data)
-        expected = list(zip(self.base, self.base))
-        for i, (x, y) in enumerate(zip(data, expected)):
-            self.assertEqual(x, y)
-            self.assertEqual(data[i], y)
+    def test_supports_random_access(self):
+        self.assertSequenceEqual(self.data, list(self.base) * self.n)
 
-        with self.assertRaises(IndexError):
-            data[len(data)]
 
-        result = data.map(lambda x: x).map(lambda x: x)
-        for i, (x, y) in enumerate(zip(result, expected)):
-            self.assertEqual(x, y)
-            self.assertEqual(result[i], y)
-        self.assertIsInstance(result._dataset, RandomAccessZip)
+class LineflowZipTestCase(TestCase):
 
-    def test_filter(self):
+    def setUp(self):
+        self.base = range(100)
+        self.n = 5
+        self.data = lineflow.zip(*[Dataset(self.base)] * self.n)
+
+    def test_returns_zip_dataset(self):
+        self.assertIsInstance(self.data, ZipDataset)
+        self.assertIsInstance(self.data._dataset, RandomAccessZip)
+
+    def test_keeps_original_dataset_after_multiple_maps(self):
+        def f(x): return x
+
+        data = self.data
+        for i in range(100):
+            data = data.map(f)
+            self.assertIsInstance(data._dataset, RandomAccessZip)
+            self.assertEqual(len(data._funcs), i + 1)
+
+    def test_supports_random_access(self):
+        self.assertSequenceEqual(self.data, list(zip(*[self.base for _ in range(self.n)])))
+
+
+class LineflowFilterTestCase(TestCase):
+
+    def setUp(self):
+        self.data = Dataset(range(100))
+
+    def test_returns_filtered_data_eagerly(self):
         result = lineflow.filter(lambda x: x % 2 == 0, self.data)
         expected = [x for x in self.data if x % 2 == 0]
         self.assertListEqual(result, expected)
 
+    def test_returns_filtered_data_lazily(self):
         result = lineflow.filter(lambda x: x % 2 == 0, self.data, lazy=True)
+        self.assertIsInstance(result, filter)
+        expected = [x for x in self.data if x % 2 == 0]
         for x, y in zip(result, expected):
             self.assertEqual(x, y)
 
-    def test_flat_map(self):
+
+class LineflowFlatMapTestCase(TestCase):
+
+    def setUp(self):
+        self.data = Dataset(range(100))
+
+    def test_returns_flat_mapped_data_eagerly(self):
         result = lineflow.flat_map(lambda x: [x] * 3, self.data)
         expected = [[x] * 3 for x in self.data]
         expected = [x for xs in expected for x in xs]
         self.assertListEqual(result, expected)
 
+    def test_returns_flat_mapped_data_lazily(self):
+        import itertools
+
         result = lineflow.flat_map(lambda x: [x] * 3, self.data, lazy=True)
+        self.assertIsInstance(result, itertools.chain)
+        expected = list(itertools.chain.from_iterable(
+            [[x] * 3 for x in self.data]))
         for x, y in zip(result, expected):
             self.assertEqual(x, y)
