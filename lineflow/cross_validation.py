05394d67a3b4c4cf112fbe433a4b1276a444b0d0
yasufumi <yasufumi.taniguchi@gmail.com>
2019-06-11 16:03:43 +0900

    Add lineflow.cross_validation

---
+++ lineflow/cross_validation.py
@@ -0,0 +1,120 @@
+from typing import List, Tuple, Any, Iterator
+import random
+
+from lineflow import Dataset
+
+
+class SubDataset(Dataset):
+    def __init__(self,
+                 dataset: Dataset,
+                 start: int,
+                 finish: int,
+                 indices: List[int] = None) -> None:
+        if start < 0 or finish > len(dataset):
+            raise ValueError('subset overruns the base dataset.')
+        self._dataset = dataset
+        self._start = start
+        self._finish = finish
+        self._size = finish - start
+        if indices is not None and len(indices) != len(dataset):
+            msg = (f'order option must have the same length as the base '
+                   'dataset: len(indices) = {len(indices)} while len(dataset) = {len(dataset)}')
+            raise ValueError(msg)
+        self._indices = indices or list(range(len(dataset)))
+
+    def __len__(self) -> int:
+        return self._size
+
+    def __iter__(self) -> Iterator[Any]:
+        for index in self._indices[self._start: self._finish]:
+            yield self._dataset[index]
+
+    def get_example(self, i: int) -> Any:
+        if i >= 0:
+            if i >= self._size:
+                raise IndexError('dataset index out of range')
+            index = self._start + i
+        else:
+            if i < -self._size:
+                raise IndexError('dataset index out of range')
+            index = self._finish + i
+
+        return self._dataset[self._indices[index]]
+
+
+def split_dataset(dataset: Dataset,
+                  split_at: int,
+                  indices: List[int] = None) -> Tuple[SubDataset]:
+    n_examples = len(dataset)
+    if not isinstance(split_at, int):
+        raise TypeError(f'split_at must be int, got {type(split_at)} instead')
+    if split_at < 0:
+        raise ValueError('split_at must be non-negative')
+    if split_at > n_examples:
+        raise ValueError('split_at exceeds the dataset size')
+    subset1 = SubDataset(dataset, 0, split_at, indices)
+    subset2 = SubDataset(dataset, split_at, n_examples, indices)
+    return subset1, subset2
+
+
+def split_dataset_random(dataset: Dataset,
+                         first_size: int,
+                         seed=None) -> Tuple[SubDataset]:
+    random.seed(seed)
+    indices = list(range(len(dataset)))
+    random.shuffle(indices)
+    return split_dataset(dataset, first_size, indices)
+
+
+def split_dataset_n(dataset: Dataset,
+                    n: int,
+                    indices: List[int] = None) -> List[SubDataset]:
+    n_examples = len(dataset)
+    sub_size = n_examples // n
+    return [SubDataset(dataset, sub_size * i, sub_size * (i + 1), indices)
+            for i in range(n)]
+
+
+def split_dataset_n_random(dataset: Dataset,
+                           n: int,
+                           seed=None) -> List[SubDataset]:
+    n_examples = len(dataset)
+    sub_size = n_examples // n
+    random.seed(seed)
+    indices = list(range(len(dataset)))
+    random.shuffle(indices)
+    return [SubDataset(dataset, sub_size * i, sub_size * (i + 1), indices)
+            for i in range(n)]
+
+
+def get_cross_validation_datasets(dataset: Dataset,
+                                  n_fold: int,
+                                  indices: List[int] = None) -> List[Tuple[SubDataset]]:
+    if indices is None:
+        indices = list(range(len(dataset)))
+    else:
+        indices = indices.copy()
+
+    whole_size = len(dataset)
+    borders = [whole_size * i // n_fold for i in range(n_fold + 1)]
+    test_sizes = [borders[i + 1] - borders[i] for i in range(n_fold)]
+
+    splits = []
+    for test_size in reversed(test_sizes):
+        size = whole_size - test_size
+        splits.append(split_dataset(dataset, size, indices))
+        new_indices = [None] * len(indices)
+        new_indices[:test_size] = indices[-test_size:]
+        new_indices[test_size:] = indices[:-test_size]
+        indices = new_indices
+
+    return splits
+
+
+def get_cross_validation_datasets_random(dataset: Dataset,
+                                         n_fold: int,
+                                         seed=None) -> List[Tuple[SubDataset]]:
+    random.seed(seed)
+    indices = list(range(len(dataset)))
+    random.shuffle(indices)
+    return get_cross_validation_datasets(dataset, n_fold, indices)
