dbd333788e6bb31d098800c7a9fd4b59cb53f4c6
yasufumi <yasufumi.taniguchi@gmail.com>
2019-06-27 10:09:50 +0900

    Make lineflow_window as lineflow.window

--- lineflow/__init__.py
+++ lineflow/__init__.py
@@ -6,6 +6,7 @@ from lineflow.core import lineflow_concat as concat  # NOQA
 from lineflow.core import lineflow_zip as zip  # NOQA
 from lineflow.core import lineflow_filter as filter  # NOQA
 from lineflow.core import lineflow_flat_map as flat_map  # NOQA
+from lineflow.core import lineflow_window as window  # NOQA
 from lineflow.core import lineflow_load as load  # NOQA
 
 from lineflow.cross_validation import split_dataset  # NOQA
