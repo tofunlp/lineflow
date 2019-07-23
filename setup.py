7cb261a90316692bd21a546df02867e1e5ffbd31
yasufumi <yasufumi.taniguchi@gmail.com>
2019-07-23 14:21:05 +0900

    Update description in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='lineflow',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    description='Framework-Agnostic NLP Data Loader in Python',
+    description='Lightweight NLP Data Loader for All Deep Learning Frameworks in Python',
     long_description=open('./README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/yasufumy/lineflow',
