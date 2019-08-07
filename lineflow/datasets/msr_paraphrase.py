5c13119f28e82cbe97b3d043fa965c7207867ed1
yasufumi <yasufumi.taniguchi@gmail.com>
2019-08-07 14:22:15 +0900

    Update msr_paraphrase.py

--- lineflow/datasets/msr_paraphrase.py
+++ lineflow/datasets/msr_paraphrase.py
@@ -1,28 +1,45 @@
-import easyfile
+from typing import Dict, List
+import os
+import io
+import csv
+import pickle
 
 from lineflow import Dataset
-from lineflow.download import cached_download
+from lineflow import download
 
 
-TRAIN_URL = 'https://raw.githubusercontent.com/wasiahmad/paraphrase_identification/master/dataset/msr-paraphrase-corpus/msr_paraphrase_train.txt'  # NOQA
-TEST_URL = 'https://raw.githubusercontent.com/wasiahmad/paraphrase_identification/master/dataset/msr-paraphrase-corpus/msr_paraphrase_test.txt'  # NOQA
+def get_msr_paraphrase() -> Dict[str, List[Dict[str, str]]]:
+
+    url = 'https://raw.githubusercontent.com/wasiahmad/paraphrase_identification/master/dataset/msr-paraphrase-corpus/msr_paraphrase_{}.txt'  # NOQA
+    root = download.get_cache_directory(os.path.join('datasets', 'msr_paraphrase'))
+
+    def creator(path):
+        dataset = {}
+        fieldnames = ('quality', 'id1', 'id2', 'string1', 'string2')
+        for split in ('train', 'test'):
+            data_path = download.cached_download(url.format(split))
+            with io.open(data_path, 'r', encoding='utf-8') as f:
+                f.readline()  # skip header
+                reader = csv.DictReader(f, delimiter='\t', fieldnames=fieldnames)
+                dataset[split] = [dict(row) for row in reader]
+
+        with io.open(path, 'wb') as f:
+            pickle.dump(dataset, f)
+        return dataset
+
+    def loader(path):
+        with io.open(path, 'rb') as f:
+            return pickle.load(f)
+
+    pkl_path = os.path.join(root, 'msr_paraphrase.pkl')
+    return download.cache_or_load_file(pkl_path, creator, loader)
 
 
 class MsrParaphrase(Dataset):
     def __init__(self,
                  split: str = 'train') -> None:
-        if split == 'train':
-            path = cached_download(TRAIN_URL)
-        elif split == 'test':
-            path = cached_download(TEST_URL)
-        else:
-            raise ValueError(f"only 'train' and 'test' are valid for 'split', but '{split}' is given.")
+        if split not in ('train', 'test'):
+            raise ValueError(f"only 'train', 'dev' and 'test' are valid for 'split', but '{split}' is given.")
 
-        fieldnames = ('quality', 'id1', 'id2', 'string1', 'string2')
-        dataset = easyfile.CsvFile(path,
-                                   encoding='utf-8',
-                                   header=True,
-                                   delimiter='\t',
-                                   fieldnames=fieldnames)
-
-        super().__init__(dataset)
+        raw = get_msr_paraphrase()
+        super().__init__(raw[split])
