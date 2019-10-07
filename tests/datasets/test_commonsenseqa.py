d6e95e61a67ec87a0fd2ae5a3154997b5ac1ffea
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2019-10-07 14:30:14 +0900

    Added commonsenseqa dataset w/ test.

---
+++ tests/datasets/test_commonsenseqa.py
@@ -0,0 +1,45 @@
+import tempfile
+import shutil
+from unittest import TestCase
+from unittest import mock
+
+from lineflow import download
+from lineflow.datasets.commonsenseqa import CommonsenseQA, get_commonsenseqa
+
+
+class CommonsenseQATestCase(TestCase):
+
+    def setUp(self):
+        self.default_cache_root = download.get_cache_root()
+        self.temp_dir = tempfile.mkdtemp()
+        download.set_cache_root(self.temp_dir)
+
+    def tearDown(self):
+        download.set_cache_root(self.default_cache_root)
+        shutil.rmtree(self.temp_dir)
+
+    def test_get_commonsenseqa(self):
+        raw = get_commonsenseqa()
+        self.assertIn("train", raw)
+        self.assertIn("dev", raw)
+        self.assertIn("test", raw)
+
+    def test_get_commonsenseqa_twice(self):
+        get_commonsenseqa()
+        with mock.patch("lineflow.datasets.commonsenseqa.pickle", autospec=True) as mock_pickle:
+            get_commonsenseqa()
+        mock_pickle.dump.assert_not_called()
+        self.assertEqual(mock_pickle.load.call_count, 1)
+
+    def test_loads_each_split(self):
+        train = CommonsenseQA(split="train")
+        dev = CommonsenseQA(split="dev")
+        test = CommonsenseQA(split="test")
+
+        self.assertEqual(len(train), 9741)
+        self.assertEqual(len(dev), 1221)
+        self.assertEqual(len(test), 1140)
+
+    def test_raises_value_error_with_invalid_split(self):
+        with self.assertRaises(ValueError):
+            CommonsenseQA(split="invalid_split")
