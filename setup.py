d7731b092e239e81eaab623ca1ff3462fa7e85bd
yasufumi <yasufumi.taniguchi@gmail.com>
2019-10-19 15:00:20 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     install_requires=['easyfile', 'gdown'],
     tests_require=['pytest'],
