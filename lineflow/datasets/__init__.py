145f73c790d989dbb269decc11f5e66c6ce65130
yasufumi <yasufumi.taniguchi@gmail.com>
2020-01-20 08:57:05 +0900

    Add CoNLL200 dataset

--- lineflow/datasets/__init__.py
+++ lineflow/datasets/__init__.py
@@ -1,4 +1,6 @@
 from .cnn_dailymail import CnnDailymail, get_cnn_dailymail  # NOQA
+from .commonsenseqa import CommonsenseQA, get_commonsenseqa  # NOQA
+from .conll2000 import Conll2000, get_conll2000  # NOQA
 from .imdb import Imdb, get_imdb  # NOQA
 from .msr_paraphrase import MsrParaphrase, get_msr_paraphrase  # NOQA
 from .penn_treebank import PennTreebank, get_penn_treebank  # NOQA
@@ -8,4 +10,3 @@ from .text_classification import get_text_classification_dataset  # NOQA
 from .text_classification import AgNews, SogouNews, Dbpedia, YelpReviewPolarity, YelpReviewFull, YahooAnswers, AmazonReviewPolarity, AmazonReviewFull  # NOQA
 from .wikitext import WikiText2, WikiText103, get_wikitext  # NOQA
 from .wmt14 import Wmt14, get_wmt14  # NOQA
-from .commonsenseqa import CommonsenseQA, get_commonsenseqa  # NOQA
