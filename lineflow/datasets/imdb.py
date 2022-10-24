1ff56d33fe279b47e24541b86447f80852c62795
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-24 01:44:03 +0000

    Adding tarfile member sanitization to extractall()

--- lineflow/datasets/imdb.py
+++ lineflow/datasets/imdb.py
@@ -20,7 +20,26 @@ def get_imdb() -> Dict[str, List[str]]:
         archive_path = gdown.cached_download(url)
         with tarfile.open(archive_path, 'r') as archive:
             print(f'Extracting to {root}...')
-            archive.extractall(root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, root)
 
         extracted_path = os.path.join(root, 'aclImdb')
 
--- lineflow/datasets/text_classification.py
+++ lineflow/datasets/text_classification.py
@@ -60,7 +60,26 @@ def get_text_classification_dataset(key) -> Dict[str, Union[List, arrayfiles.Csv
 
         with tarfile.open(archive_path, 'r') as archive:
             print(f'Extracting to {root}...')
-            archive.extractall(root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, root)
 
         dataset = {}
         for split in ('train', 'test'):
